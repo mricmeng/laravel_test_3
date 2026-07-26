ba88d2bda30a792c90d3215348f5e6411e172a99
Meng Chomraoen <chamraoenmeng@gmail.com>
2026-07-27 00:14:07 +0700

    [update] pagination

--- i_mric_laravel_dashboard/app/Http/Controllers/productController.php
+++ i_mric_laravel_dashboard/app/Http/Controllers/productController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\productModel;
 use Illuminate\Http\Request;
 
 class productController extends Controller
 {
     public function index(){
-        
-        return view('product');
+        $products = productModel::orderBy('id', 'DESC')->paginate(8);
+      
+        return view('product', [
+            'products' => $products,
+        ]);
     }
 
     public function create(){
