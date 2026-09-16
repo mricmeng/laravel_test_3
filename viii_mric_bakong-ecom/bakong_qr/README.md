9c66188b2cfcce7093826bab3addcc098e59dcac
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-09-16 22:30:14 +0700

    Update

--- viii_mric_bakong-ecom/bakong_qr/README.md
+++ viii_mric_bakong-ecom/bakong_qr/README.md
@@ -1,9 +1,9 @@
-# step 1 _/ install laravel/
+#### 1️⃣ Project Setup Laravel
 ### បង្កើត folder payment
 ## -composer global require laravel/installer 
 ### -laravel new bakong_qr
 ### -cd bakong_qr
-### -php artisan serve
+####### -php artisan serve
 
 # step 2 _/install khqr-gateway/bakong-khqr-php:
 ### -open chrome url: ( https://packagist.org/packages/khqr-gateway/bakong-khqr-php )
---
+++ viii_mric_bakong-ecom/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7a988ae8b600ac3d351b8a4ba6de82ac144584d6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7a988ae8b600ac3d351b8a4ba6de82ac144584d6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'khqr-gateway/bakong-khqr-php' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '1df08d33ebb85b3b6b284205c422c3d6d8433973',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../khqr-gateway/bakong-khqr-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
